ad8f7731b34608c850584d674e130c9b32f7a95b
David Robertson <david@robertson.yt>
2021-06-27 16:10:41 +0100

    Pulled config out into json file, removed ping command, and made bot send one combined message when replying to a message with multiple currency values.

--- app.js
+++ app.js
@@ -1,211 +1,197 @@
-// config
-var config = {
-	"commandPrefix": "!",
-	"currencyRateTTL": 12 * 60 * 60 * 1000,
-	"selectedCurrencies": [
-		"EUR",
-		"GBP",
-		"CHF",
-		"RON",
-		"USD",
-		"AUD",
-		"CAD",
-		"PLN",
-		"HUF",
-                "SEK"
-	],
-	"ignoreCurrencies": [
-		"ALL",
-		"AMD",
-		"BSD",
-		"GHS",
-		"MOP",
-		"TOP",
-		"TRY",
-		"CLP",
-		"CUP"
-	]
-}
-
-
-// // // // // //
-
 require('dotenv').config()
+let config = require("./config.json");
 
 const util = require('util');
-
-const Discord = require("discord.js");
-const client = new Discord.Client();
 
 const money = require("money");
 const oxr = require("open-exchange-rates");
 oxr.set({app_id: process.env.OXR_APP_ID});
 const oxrLatestAsync = util.promisify(oxr.latest);
-var ratesLastUpdated = 0;
+let ratesLastUpdated = 0;
+
 async function updateRates() {
-	if ((Date.now() - ratesLastUpdated) > config["currencyRateTTL"]) {
-		console.log("Updating exchange rates...");
-		await oxrLatestAsync().then(function() {
-			money.base = oxr.base;
-			money.rates = oxr.rates;
-			ratesLastUpdated = Date.now();
-		});
-	} else {
-		console.log("Skipping update of exchange rates.");
-	}
+    if ((Date.now() - ratesLastUpdated) > config["currencyRateTTL"]) {
+        console.log("Updating exchange rates...");
+        await oxrLatestAsync().then(function () {
+            money.base = oxr.base;
+            money.rates = oxr.rates;
+            ratesLastUpdated = Date.now();
+        });
+    } else {
+        console.log("Skipping update of exchange rates.");
+    }
 }
+
 updateRates();
-
-// // // // // // // //
-
 
 
 // DISCORD INIT //
-client.on("ready", () => {
-	console.log(`MoneyMan has started, with ${client.users.size} users, in ${client.channels.size} channels of ${client.guilds.size} servers.`);
+const Discord = require("discord.js");
+const client = new Discord.Client();
+client.on("ready", async () => {
+    console.log(`MoneyMan has started, with ${client.users.cache.size} users, in ${client.channels.cache.size} channels of ${client.guilds.cache.size} servers.`);
 
-	client.generateInvite(804781379)
-  	.then(link => console.log(link))
-  	.catch(console.error);
+    const link = await client.generateInvite({
+        "permissions": [
+            "VIEW_CHANNEL",
+            "CHANGE_NICKNAME",
+            "SEND_MESSAGES",
+            "MANAGE_MESSAGES",
+            "EMBED_LINKS",
+            "ATTACH_FILES",
+            "READ_MESSAGE_HISTORY",
+            "ADD_REACTIONS",
+            "USE_EXTERNAL_EMOJIS"
+        ]
+    })
+    console.log(link)
 
-	client.user.setActivity('Chemistry HQ', {type: 'WATCHING'});
+    //client.user.setActivity('Chemistry HQ', {type: 'WATCHING'});
+    await client.user.setActivity();
 });
 client.on("guildCreate", guild => {
-	console.log(`Joined server: ${guild.name} (id: ${guild.id}).`);
+    console.log(`Joined server: ${guild.name} (id: ${guild.id}).`);
 });
 client.on("guildDelete", guild => {
-	console.log(`Removed from server: ${guild.name} (id: ${guild.id})`);
+    console.log(`Removed from server: ${guild.name} (id: ${guild.id})`);
 });
+client.login(process.env.DISCORD_TOKEN);
 
 // COMMANDS //
 const commands = {
-	"ping": async function(client, message, args) {
-		// Calculates ping between sending a message and editing it, giving a nice round-trip latency.
-		// The second ping is an average latency between the bot and the websocket server (one-way, not round-trip)
-		const m = await message.channel.send("...");
-		m.edit(`Pong! Latency is ${m.createdTimestamp - message.createdTimestamp}ms. API Latency is ${Math.round(client.ping)}ms`);
-	},
-	"forceRateUpdate": async function(client, message, args) {
-		ratesLastUpdated = 0;
-		await updateRates();
-		message.channel.send("Rates updated from OXR.");
-	},
+    "forceRateUpdate": async function (client, message, args) {
+        ratesLastUpdated = 0;
+        await updateRates();
+        message.channel.send("Rates updated from OXR.");
+    }
 }
 client.on("message", async message => {  // This event will run on every single message received, from any channel or DM.
-	// Ignore messages from bots (including self)
-	if(message.author.bot) return;
+    // Ignore messages from bots (including self)
+    if (message.author.bot) {
+        return;
+    }
 
-	// Ignore messages that don't start with our prefix
-	if(message.content.indexOf(config["commandPrefix"]) !== 0) return;
+    // Ignore messages that don't start with our prefix
+    if (message.content.indexOf(config["commandPrefix"]) !== 0) {
+        return;
+    }
 
-	// Split up the command/args
-	const args = message.content.slice(config["commandPrefix"].length).trim().split(/ +/g);
-	const command = args.shift().toLowerCase();
+    // Split up the command/args
+    const args = message.content.slice(config["commandPrefix"].length).trim().split(/ +/g);
+    const command = args.shift().toLowerCase();
 
-	if (commands.hasOwnProperty(command)) {
-		var commandFunction = commands[command];
-		return await commandFunction(client, message, args);
-	} else {
-		// unrecognized command
-	}
+    if (commands.hasOwnProperty(command)) {
+        var commandFunction = commands[command];
+        return await commandFunction(client, message, args);
+    } else {
+        // unrecognized command
+    }
 });
 
 // INLINE CURRENCY CONVERSION //
 client.on("message", async message => {
-	// Ignore messages from bots (including self)
-	if(message.author.bot) return;
+    // Ignore messages from bots (including self)
+    if (message.author.bot) {
+        return;
+    }
 
-	var currencyMentions = scanForCurrencyMentions(message.content);
-	if (currencyMentions > 0) {
-		console.log("Got currency mentions: ", currencyMentions);
-		updateRates();
-	}
-	for (currencyMention of currencyMentions) {
-		var split = currencyMention.split(" ");
-		var fromCurrency = split[0];
-		if (Object.keys(money.rates).includes(fromCurrency)) {
-			var reply = buildCurrencyReply(currencyMention);
-			message.channel.send(reply);
-		}
-	}
+    const currencyMentions = scanForCurrencyMentions(message.content);
+    if (currencyMentions > 0) {
+        console.log("Got currency mentions: ", currencyMentions);
+        await updateRates();
+    }
+
+    let repliesToSend = []
+
+    for (const currencyMention of currencyMentions) {
+        const split = currencyMention.split(" ");
+        const fromCurrency = split[0];
+        if (Object.keys(money.rates).includes(fromCurrency)) {
+            repliesToSend.push(buildCurrencyReply(currencyMention));
+        }
+    }
+
+    if (repliesToSend.length !== 0) {
+        const reply = repliesToSend.join("\n")
+        await message.channel.send(reply);
+    }
 });
 
 function buildCurrencyReply(currencyMention) {
-	console.log(`Building currency reply for input '${currencyMention}'`);
-	var split = currencyMention.split(" ");
-	var fromCurrency = split[0];
-	var fromAmount = Number.parseFloat(split[1]);
+    console.log(`Building currency reply for input '${currencyMention}'`);
+    const split = currencyMention.split(" ");
+    const fromCurrency = split[0];
+    const fromAmount = Number.parseFloat(split[1]);
 
-	var targetResults = [];
-	for (targetCurrency of config["selectedCurrencies"]) {
-		if (targetCurrency != fromCurrency) {
-			var targetAmount = money.convert(fromAmount, {from: fromCurrency, to: targetCurrency});
-			targetResults.push(`${targetAmount.toFixed(2)} ${targetCurrency}`);
-		}
-	}
-	targetResultString = targetResults.join(", or ");
-
-	var reply = `${fromAmount.toFixed(2)} ${fromCurrency} is worth ${targetResultString}.`;
-	return reply;
+    let targetResults = [];
+    let targetCurrency;
+    for (targetCurrency of config["selectedCurrencies"]) {
+        if (targetCurrency !== fromCurrency) {
+            const targetAmount = money.convert(fromAmount, {from: fromCurrency, to: targetCurrency});
+            targetResults.push(`${targetAmount.toFixed(2)} ${targetCurrency}`);
+        }
+    }
+    const targetResultString = targetResults.join(", or ");
+    const reply = `${fromAmount.toFixed(2)} ${fromCurrency} is worth ${targetResultString}.`;
+    return reply;
 }
 
 function scanForCurrencyMentions(msg) {
-	result = [];
+    let amount;
+    let code;
+    let match;
+    let result = [];
 
-	// £12.34 $12.34 €12.34
-	var symbolPrefixMatches = msg.matchAll(/([£€$])\s?(\d+(?:\.\d{1,2})?)/g);
-	for (match of symbolPrefixMatches) {
-		var code = currencySymbolToCode(match[1]);
-		var amount = Number.parseFloat(match[2]);
-		result.push([code, amount].join(" "));
-	}
+    // £12.34 $12.34 €12.34
+    const symbolPrefixMatches = msg.matchAll(/([£€$])\s?(\d+(?:\.\d{1,2})?)/g);
+    for (match of symbolPrefixMatches) {
+        code = currencySymbolToCode(match[1]);
+        amount = Number.parseFloat(match[2]);
+        result.push([code, amount].join(" "));
+    }
 
-	// 12.34$ 12.34€
-	var symbolSuffixMatches = msg.matchAll(/(\d+(?:\.\d{1,2})?)\s?([€$])/g);
-	for (match of symbolSuffixMatches) {
-		var code = currencySymbolToCode(match[2]);
-		var amount = Number.parseFloat(match[1]);
-		result.push([code, amount].join(" "));
-	}
+    // 12.34$ 12.34€
+    const symbolSuffixMatches = msg.matchAll(/(\d+(?:\.\d{1,2})?)\s?([€$])/g);
+    for (match of symbolSuffixMatches) {
+        code = currencySymbolToCode(match[2]);
+        amount = Number.parseFloat(match[1]);
+        result.push([code, amount].join(" "));
+    }
 
-	// GBP 12.34
-	var codePrefixMatches = msg.matchAll(/(?<!\w)([a-zA-Z]{3})\s?(\d+(?:\.\d{1,2})?)/g);
-	for (match of codePrefixMatches) {
-		var code = match[1].toUpperCase();
-		var amount = Number.parseFloat(match[2]);
-		result.push([code, amount].join(" "));
-	}
+    // GBP 12.34
+    const codePrefixMatches = msg.matchAll(/(?<!\w)([a-zA-Z]{3})\s?(\d+(?:\.\d{1,2})?)/g);
+    for (match of codePrefixMatches) {
+        code = match[1].toUpperCase();
+        amount = Number.parseFloat(match[2]);
+        result.push([code, amount].join(" "));
+    }
 
-	// 12.34 GBP
-	var codeSuffixMatches = msg.matchAll(/(\d+(?:\.\d{1,2})?)\s?([a-zA-Z]{3})(?!\w)/g);
-	for (match of codeSuffixMatches) {
-		var code = match[2].toUpperCase();
-		var amount = Number.parseFloat(match[1]);
-		result.push([code, amount].join(" "));
-	}
+    // 12.34 GBP
+    const codeSuffixMatches = msg.matchAll(/(\d+(?:\.\d{1,2})?)\s?([a-zA-Z]{3})(?!\w)/g);
+    for (match of codeSuffixMatches) {
+        code = match[2].toUpperCase();
+        amount = Number.parseFloat(match[1]);
+        result.push([code, amount].join(" "));
+    }
 
-	// Strip out duplicates
-	result = [... new Set(result)];
+    // Strip out duplicates
+    result = [...new Set(result)];
 
-	// Filter out ignored currencies
-	result = result.filter(function (currencyStr) {
-		return !config["ignoreCurrencies"].some(currency => currencyStr.startsWith(currency));
-	});
+    // Filter out ignored currencies
+    result = result.filter(function (currencyStr) {
+        return !config["ignoreCurrencies"].some(currency => currencyStr.startsWith(currency));
+    });
 
-	// return the array of strings, each in the format of "GBP 12.34"
-	return result;
+    // return the array of strings, each in the format of "GBP 12.34"
+    return result;
 }
 
 function currencySymbolToCode(symbol) {
-	if (symbol == "£") {
-		return "GBP";
-	} else if (symbol == "€") {
-		return "EUR";
-	} else if (symbol == "$") {
-		return "USD";
-	}
+    if (symbol === "£") {
+        return "GBP";
+    } else if (symbol === "€") {
+        return "EUR";
+    } else if (symbol === "$") {
+        return "USD";
+    }
 }
-
-
-// DISCORD LOGIN //
-client.login(process.env.DISCORD_TOKEN);
